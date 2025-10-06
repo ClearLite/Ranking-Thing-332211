5ef2a1c816d8313967dfcfafde0dce43c1d45faf
ClearLite <109916578+ClearLite@users.noreply.github.com>
2025-10-05 20:14:59 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 # run.py
 
-from project import create_app
+from project import create_app, db
+from flask_migrate import Migrate
 
+# Create the Flask app instance
 app = create_app()
+
+# Initialize Flask-Migrate
+# This line is CRITICAL for the 'flask db' command to work
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)
